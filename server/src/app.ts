6997f21ebdd842024ecc3be8884a432846488fc3
Dharshini <95228786+Dharshini231001@users.noreply.github.com>
2026-02-08 20:18:22 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,21 +2,25 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes";
 import fieldRoutes from "./routes/fieldRoutes";
-import path from 'path';
+import path from "path";
+import { fileURLToPath } from "url";  
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);    
+
 const app = express();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/fields", fieldRoutes);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '../client/user_management_portal/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/user_management_portal/build/index.html'));
-});
 
+app.use(express.static(path.join(__dirname, "../client/user_management_portal/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/user_management_portal/build/index.html"));
+});
 
 export default app;
