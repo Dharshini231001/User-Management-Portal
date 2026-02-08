3bfa15348919675749898811f340e1abefc0fbf9
Dharshini <95228786+Dharshini231001@users.noreply.github.com>
2026-02-08 20:38:11 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI as string;
+const MONGO_URI = process.env.PORT || 3000;
 
 if (!MONGO_URI) {
   throw new Error("MONGO_URI is not defined in .env");
@@ -15,7 +14,6 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
