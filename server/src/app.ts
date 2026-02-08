b27a884b5e42b50e58bb7be0bd23a39afaa7fcb0
Dharshini <95228786+Dharshini231001@users.noreply.github.com>
2026-02-08 19:35:19 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,4 +11,14 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/fields", fieldRoutes);
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`server at http://localhost:${port}`);
+});
 export default app;
